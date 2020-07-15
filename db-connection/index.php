a8fb2c0736b93ce9d73b82975fd2089449ef659e
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-07-15 14:38:35 -0400

    adding users to users table with insert query

--- db-connection/index.php
+++ db-connection/index.php
@@ -1,10 +1,40 @@
 <?php
 
 
+
+
+
+
+
+
+
+
+<?php
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "test";
 
 
+// Create connection
+
 $connection = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+
+// Check connection
+
+if (!$connection) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+ 
+$sql = INSERT INTO users (FirstName, LastName, Email)
+VALUES ("test", "Doe", "Email");
+if (mysqli_query($connection, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+}
+mysqli_close($connection);
+
+?>
